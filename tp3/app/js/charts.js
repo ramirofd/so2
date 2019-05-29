e213cc1fe6f6665731ed8a5756de94697d359ae2
Ramiro Detke <ramiro.fer.det@gmail.com>
2019-05-29 18:07:58 -0300

    agregados archivos app web

---
+++ tp3/app/js/charts.js
@@ -0,0 +1,144 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var memoryChartOpt = {
+        chart: {
+            height: 500,
+            type: 'radialBar',
+        },
+        plotOptions: {
+            radialBar: {
+                startAngle: -135,
+                endAngle: 135,
+                dataLabels: {
+                    name: {
+                        fontSize: '16px',
+                        color: undefined,
+                        offsetY: 120
+                    },
+                    value: {
+                        offsetY: 76,
+                        fontSize: '22px',
+                        color: undefined,
+                        formatter: function (val) {
+                            return val + "%";
+                        }
+                    }
+                }
+            }
+        },
+        fill: {
+            type: 'gradient',
+            gradient: {
+                shade: 'dark',
+                shadeIntensity: 0.15,
+                inverseColors: false,
+                opacityFrom: 1,
+                opacityTo: 1,
+                stops: [0, 50, 65, 91]
+            },
+        },
+        stroke: {
+            dashArray: 2
+        },
+        series: [0],
+        labels: ['Used Memory'],
+    };
+    
+    var cpuChartOpt = {
+        chart: {
+            height: 500,
+            type: 'radialBar',
+        },
+
+        plotOptions: {
+            radialBar: {
+                startAngle: -135,
+                endAngle: 135,
+                dataLabels: {
+                    name: {
+                        fontSize: '16px',
+                        color: undefined,
+                        offsetY: 120
+                    },
+                    value: {
+                        offsetY: 76,
+                        fontSize: '22px',
+                        color: undefined,
+                        formatter: function (val) {
+                            return val + "%";
+                        }
+                    }
+                }
+            }
+        },
+        fill: {
+            type: 'gradient',
+            gradient: {
+                shade: 'dark',
+                shadeIntensity: 0.15,
+                inverseColors: false,
+                opacityFrom: 1,
+                opacityTo: 1,
+                stops: [0, 50, 65, 91]
+            },
+        },
+        stroke: {
+            dashArray: 2
+        },
+        series: [0],
+        labels: ['Used CPU'],
+    };
+    var xhr = null;
+
+    getXmlHttpRequestObject = function()
+    {
+        if(!xhr)
+        {               
+            // Create a new XMLHttpRequest object 
+            xhr = new XMLHttpRequest();
+        }
+        return xhr;
+    };
+
+    updateLiveData = function()
+    {
+        var now = new Date();
+        // Date string is appended as a query with live data 
+        // for not to use the cached version 
+        var url = 'http://192.168.0.71/cgi-bin/hello.cgi';
+        xhr = getXmlHttpRequestObject();
+        xhr.onreadystatechange = evenHandler;
+        // asynchronous requests
+        xhr.open("GET", url, true);
+        // Send the request over the network
+        xhr.send(null);
+    };
+
+    updateLiveData();
+
+    function evenHandler()
+    {
+        // Check response is ready or not
+        if(xhr.readyState == 4 && xhr.status == 200)
+        {
+            dataDiv = document.getElementById('liveData');
+            // Set current data text
+            dataDiv.innerHTML = xhr.responseText;
+            // Update the live data every 1 sec
+            setTimeout(updateLiveData(), 1000);
+        }
+    }
+    var memoryChart = new ApexCharts(document.querySelector("#memoryChart"), memoryChartOpt);
+    memoryChart.render();
+    
+    var cpuChart = new ApexCharts(document.querySelector("#cpuChart"), cpuChartOpt);
+    cpuChart.render();
+    
+ }, false);
+ 
+function updateCpuChart(){
+    cpuChart.updateSeries([Math.floor(Math.random() * 101)], true)
+}
+
+function updateMemoryChart(){
+    memoryChart.updateSeries([Math.floor(Math.random() * 101)], true)
+}
