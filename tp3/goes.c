e2c4458c8b231a599bd9a014e34df5e429a35586
Ramiro Detke <ramiro.fer.det@gmail.com>
2019-05-30 00:28:53 -0300

    goes json

--- tp3/goes.c
+++ tp3/goes.c
@@ -8,13 +8,16 @@ int main(void)
 	printf("Content-Type: application/json \n\n");
 	char * line = NULL;
 	char buffer[500];
+	char response[200000];
 	size_t len = 0;
 	ssize_t read;
 	FILE * command;
 	command = popen("aws s3 ls s3://noaa-goes16/ABI-L2-CMIPF/2017/360 --recursive --no-sign-request --human-readable | grep OR_ABI-L2-CMIPF-M3C13_G16", "r");
 	printf("{\n\t\"data\":[\n\t\t");
+	memset(response, '\0', 200000);
 	while((read = getline(&line, &len, command)) != -1)
-	{	memset(buffer, '\0', 500);
+	{	
+		memset(buffer, '\0', 500);
 		line[read-1] = '\0';
 		strcat(buffer, "{");
 		char * text = strtok(line," ");
@@ -46,8 +49,9 @@ int main(void)
 		strcat(buffer, "\"");
 
 		strcat(buffer, "},\n\t\t");
-		printf("%s", buffer);
+		strcat(response, buffer);
 	}
+	printf("%s", response);
 	printf("]\n}");
 	return 0;
 }
