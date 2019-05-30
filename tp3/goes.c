37079d880f3490d9bbb7e5bea0909cafe8bdfc4b
Ramiro Detke <ramiro.fer.det@gmail.com>
2019-05-30 03:56:01 -0300

    goes json

--- tp3/goes.c
+++ tp3/goes.c
@@ -7,46 +7,27 @@ int main(void)
 {
 	printf("Content-Type: text/plain\n\n");
 	char * line = NULL;
+	char * word, * word1;
 	char buffer[500];
 	size_t len = 0;
 	ssize_t read;
 	FILE * command;
 	command = popen("aws s3 ls s3://noaa-goes16/ABI-L2-CMIPF/2017/360 --recursive --no-sign-request --human-readable | grep OR_ABI-L2-CMIPF-M3C13_G16", "r");
+	printf("{\n\t\"data\":\n\t\t[");
 	while((read = getline(&line, &len, command)) != -1)
-	{	memset(buffer, '\0', 500);
-		line[read-1] = '\0';
-		strcat(buffer, "<tr>\n");
-		char * text = strtok(line," ");
-		strcat(buffer, "\t<td>");
-		strcat(buffer, text);
-		strcat(buffer, "</td>\n");
-		
-		text = strtok(NULL, " ");
-		strcat(buffer, "\t<td>");
-		strcat(buffer, text);
-		strcat(buffer, "</td>\n");
-		
-		text = strtok(NULL, " ");
-		strcat(buffer, "\t<td>");
-		strcat(buffer, text);
-		strcat(buffer, " ");
-		text = strtok(NULL, " ");
-		strcat(buffer, text);
-		strcat(buffer, "</td>\n");
-
-		text = strtok(NULL, " ");
-		text = strtok(text, "/");
-		text = strtok(NULL, "/");
-		text = strtok(NULL, "/");
-		text = strtok(NULL, "/");
-		text = strtok(NULL, "/");
-		strcat(buffer, "\t<td>");
-		strcat(buffer, text);
-		strcat(buffer, "</td>\n");
-
-		strcat(buffer, "</tr>\n");
-		printf("%s", buffer);
+	{	
+		line[strlen(line)-1] = '\0';
+		word = strtok(line, " ");
+		printf("{\"date\":\"%s\", ", word);
+		word = strtok(NULL, " ");
+		printf("\"time\":\"%s\", ", word);
+		word = strtok(NULL, " ");
+		word1 = strtok(NULL, " ");
+		printf("\"size\":\"%s %s\", ", word, word1);
+		word = strtok(NULL, " ");
+		printf("\"name\":\"%s\"", word);
+		printf("},\n\t\t");
 	}
-	printf("END\n");
+	printf("]\n}\n");
 	return 0;
 }
