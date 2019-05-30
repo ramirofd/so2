e72b99c79654f72bb7f4fd54b5ebaada9d5b8ecc
Ramiro Detke <ramiro.fer.det@gmail.com>
2019-05-30 04:13:50 -0300

    goes json

--- tp3/goes.c
+++ tp3/goes.c
@@ -8,17 +8,24 @@ int main(void)
 	printf("Content-Type: application/json \n\n");
 	char * line = NULL;
 	char * word, * word1;
+	int i = 0, lines = 0;
 	char buffer[500];
 	size_t len = 0;
 	ssize_t read;
 	FILE * command;
 	command = popen("aws s3 ls s3://noaa-goes16/ABI-L2-CMIPF/2017/360 --recursive --no-sign-request --human-readable | grep OR_ABI-L2-CMIPF-M3C13_G16", "r");
-	printf("{\n\t\"data\":\n\t\t[");
+	printf("{\n\t\"data\":\n\t\t{");
+	while((read = getline(&line, &len, command)) != -1)
+	{
+		lines++;
+	}
+	pclose(command);
+	command = popen("aws s3 ls s3://noaa-goes16/ABI-L2-CMIPF/2017/360 --recursive --no-sign-request --human-readable | grep OR_ABI-L2-CMIPF-M3C13_G16", "r");
 	while((read = getline(&line, &len, command)) != -1)
 	{	
 		line[strlen(line)-1] = '\0';
 		word = strtok(line, " ");
-		printf("{\"date\":\"%s\", ", word);
+		printf("\"%d\":{\"date\":\"%s\", ", i, word);
 		word = strtok(NULL, " ");
 		printf("\"time\":\"%s\", ", word);
 		word = strtok(NULL, " ");
@@ -26,8 +33,16 @@ int main(void)
 		printf("\"size\":\"%s %s\", ", word, word1);
 		word = strtok(NULL, " ");
 		printf("\"name\":\"%s\"", word);
-		printf("},\n\t\t");
+		if(i != lines-1)
+		{
+			printf("},\n\t\t");
+		}
+		else
+		{
+			printf("}\n\t\t");
+		}
+		i++;
 	}
-	printf("]\n}\n");
+	printf("}\n}\n");
 	return 0;
 }
