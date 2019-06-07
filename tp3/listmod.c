342adbd5251f71abe8f39e6c184e6bb382c4ec0b
Ramiro Detke <ramiro.fer.det@gmail.com>
2019-06-06 22:11:35 -0300

    list modules

--- tp3/listmod.c
+++ tp3/listmod.c
@@ -11,9 +11,7 @@ int main(void)
 	size_t len = 0;
 	ssize_t read;
 	FILE * command;
-	char * qs = getenv("QUERY_STRING");
-	printf("%s", qs);
-	command = popen("aws s3 ls s3://noaa-goes16/ABI-L2-CMIPF/2017/360/20 --recursive --no-sign-request --human-readable | grep OR_ABI-L2-CMIPF-M3C13_G16", "r");
+	command = popen("lsmod", "r");
 	while((read = getline(&line, &len, command)) != -1)
 	{	memset(buffer, '\0', 500);
 		line[read-1] = '\0';
@@ -33,16 +31,6 @@ int main(void)
 		strcat(buffer, text);
 		strcat(buffer, " ");
 		text = strtok(NULL, " ");
-		strcat(buffer, text);
-		strcat(buffer, "</td>\n");
-
-		text = strtok(NULL, " ");
-		text = strtok(text, "/");
-		text = strtok(NULL, "/");
-		text = strtok(NULL, "/");
-		text = strtok(NULL, "/");
-		text = strtok(NULL, "/");
-		strcat(buffer, "\t<td>");
 		strcat(buffer, text);
 		strcat(buffer, "</td>\n");
 
